5c657a6e532631d88855743816eeeca0fee4ba53
Almas2002 <73034060+Almas2002@users.noreply.github.com>
2022-04-04 14:09:40 +0600

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,17 +14,22 @@ import { Like } from './profile/like.entity';
 import { Religion } from './ religion/religion.entity';
 import { ReligionModule } from './ religion/religion.module';
 import { ProfilePhotos } from './profile/profile-photos.entity';
-
+import { ConfigModule } from '@nestjs/config';
+require('dotenv').config()
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal:true,
+      envFilePath:".env"
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '12345',
-      database: 'user',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [Category, Gender, Hobby, Profile,Complaint,Like,Religion,ProfilePhotos],
       synchronize: true,
     })
